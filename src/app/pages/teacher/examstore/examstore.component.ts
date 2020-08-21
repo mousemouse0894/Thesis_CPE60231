79543c1b651106ee5b63e189b156d9c90189bbeb
jirapat169 <james.jirapat@outlook.com>
2020-08-22 02:50:13 +0700

    Update ace.js

--- src/app/pages/teacher/examstore/examstore.component.ts
+++ src/app/pages/teacher/examstore/examstore.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+const _window: any = window;
 @Component({
   selector: 'app-examstore',
   templateUrl: './examstore.component.html',
-  styleUrls: ['./examstore.component.scss']
+  styleUrls: ['./examstore.component.scss'],
 })
 export class ExamstoreComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    _window.ace.require('ace/ext/language_tools');
 
+    var editor = _window.ace.edit('editor1');
+
+    editor.session.setMode('ace/mode/sql');
+    editor.setTheme('ace/theme/tomorrow');
+    editor.setOptions({
+      enableBasicAutocompletion: true,
+      enableSnippets: true,
+      enableLiveAutocompletion: true,
+    });
+  }
 }

3a6f614ea37aff28398c0a6ce7eacbcd2b2c21e7
mousemouse0894 <biohazardking2012@gmail.com>
2020-09-25 03:14:00 +0700

    Update_Topic

--- src/app/pages/teacher/events-exam/add-events/add-events.component.ts
+++ src/app/pages/teacher/events-exam/add-events/add-events.component.ts
@@ -1,50 +1,94 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/services/app.service';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-add-events',
   templateUrl: './add-events.component.html',
   styleUrls: ['./add-events.component.scss'],
 })
 export class AddEventsComponent implements OnInit {
-  public examResult: any = null;
-  public getunit: any = null;
-  constructor(public service: AppService) {}
+  public formInserttest: FormGroup;
+  public groupStudent: any = null;
+  public listExamSet: Array<any> = [];
+  public examView: Array<any> = [];
+  public paginationPage: number = 1;
+
+  constructor(public service: AppService, private formBuilder: FormBuilder) {}
 
   ngOnInit() {
-    this.onGetexam();
-    this.getUnittable();
+    this.formInserttest = this.formBuilder.group({
+      groupID_fk: ['', Validators.required],
+      exambodyID_fk: ['', Validators.required],
+      topicPassword: ['', Validators.required],
+      timeStart: ['', Validators.required],
+      timeEnd: ['', Validators.required],
+      topicText: ['', Validators.required],
+      owner: this.service.localStorage.get('userLogin')['uid'],
+    });
+
+    this.getExamSet();
+    this.onGetgroupstudent();
   }
 
-  public onGetexam = () => {
+  public selectExamView = (list: Array<any>) => {
+    this.examView = list;
+    console.log(list);
+  };
+
+  public getExamSet = () => {
     this.service
       .httpGet(
-        `/exstore/get/${
+        `/exbody/getExamBody/${
           this.service.localStorage.get('userLogin')['uid']
         }?token=${this.service.localStorage.get('userLogin')['token']}`
       )
+      .then((val: any) => {
+        if (val.result.length > 0) {
+          this.listExamSet = val.result;
+        } else {
+          this.listExamSet = [];
+        }
+      });
+  };
+
+  public insertTopic = () => {
+    this.service
+      .httpPost(
+        `extopic/insert?token=${
+          this.service.localStorage.get('userLogin')['token']
+        }`,
+        JSON.stringify(this.formInserttest.value)
+      )
       .then((value: any) => {
         if (value.success) {
-          this.examResult = value.result;
+          this.service.showAlert('', 'สำเร็จ', 'success');
         } else {
           this.service.showAlert('', value.message, 'error');
         }
       });
   };
-  public getUnittable = () => {
+
+  public onGetgroupstudent = () => {
+    this.groupStudent = [];
     this.service
       .httpGet(
-        '/examunit/get/' +
+        '/groupst/getowner/' +
           this.service.localStorage.get('userLogin')['uid'] +
-          '?token=' +
-          this.service.localStorage.get('userLogin')['token']
+          `?token=${this.service.localStorage.get('userLogin')['token']}`
       )
       .then((value: any) => {
         if (value.success) {
-          this.getunit = value.result;
+          this.groupStudent = value.result;
         } else {
           this.service.showAlert('', value.message, 'error');
         }
       });
+  };
+
+  public onSelectExam = (x: any) => {
+    this.formInserttest.patchValue({
+      exambodyID_fk: x.exambodyID_fk,
+    });
+    this.service.showAlert('', 'เลือกข้อสอบ' + x.topic, 'success');
   };
 }
--- src/app/pages/teacher/events-exam/events-exam.module.ts
+++ src/app/pages/teacher/events-exam/events-exam.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { EventsExamRoutingModule } from './events-exam-routing.module';
 import { EventsExamComponent } from './events-exam.component';
 import { AddEventsComponent } from './add-events/add-events.component';
-
-
+import { NgxPaginationModule } from 'ngx-pagination';
+import { SharedModule } from 'src/app/shared/shared-module';
 @NgModule({
   declarations: [EventsExamComponent, AddEventsComponent],
   imports: [
     CommonModule,
-    EventsExamRoutingModule
-  ]
+    EventsExamRoutingModule,
+    NgxPaginationModule,
+    SharedModule,
+  ],
 })
-export class EventsExamModule { }
+export class EventsExamModule {}
